28217465af5085921366861c11a3f4874ce455e1
minhtrong20130448 <20130448@st.hcmuaf.edu.vn>
2024-06-14 07:16:53 +0700

    feat: pingpong job

---
+++ Server/socket/src/main/java/vn/edu/nlu/fit/nlugame/layer0/scheduled/CronInitialization.java
@@ -0,0 +1,4 @@
+package vn.edu.nlu.fit.nlugame.layer0.scheduled;
+
+public class CronInitialization {
+}
---
+++ Server/socket/src/main/java/vn/edu/nlu/fit/nlugame/layer0/scheduled/jobs/PingPongJob.java
@@ -0,0 +1,2 @@
+package vn.edu.nlu.fit.nlugame.layer0.scheduled.jobs;public class PingPongJob {
+}
